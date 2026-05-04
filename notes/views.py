9667a1e0573181d1fad9aecb9a5d30443ab1483c
Rahdriawan Prismana <63779468+prismana@users.noreply.github.com>
2026-05-04 21:05:57 +0700

    Adding PUT feature, update note information -> adding put method in NoteDetail. Adding link to PUT method in serializers.

--- notes/views.py
+++ notes/views.py
@@ -34,3 +34,12 @@ class NoteDetail(APIView):
         note = self.get_object(pk)
         serializer = NoteSerializer(note)
         return Response(serializer.data)
+
+    # Update note by primary key
+    def put(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
