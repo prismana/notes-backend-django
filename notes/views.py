19b4f7e895ec880c60e78581d8e8179b2281a3f1
Rahdriawan Prismana <63779468+prismana@users.noreply.github.com>
2026-05-04 20:52:02 +0700

    Adding NoteDetail -> get all note detail. Adding links to serializer -> HATEOAS principle.

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+
 from notes.setializers import NoteSerializer
+from .models import Note
 
 
 class NoteList(APIView):
@@ -13,3 +15,22 @@ class NoteList(APIView):
             return Response(note.data, status=status.HTTP_201_CREATED)
 
         return Response(note.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True, context={'request': request})
+        return Response({
+            "notes": serializer.data,
+        }, status=status.HTTP_200_OK)
+
+class NoteDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
